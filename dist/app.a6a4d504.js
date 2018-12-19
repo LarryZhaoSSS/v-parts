8878f3fdcac0a0998104fb8f23f33980c8191474
LarryKameZhao <larryzhao1994@outlook.com>
2018-12-19 08:22:59 +0800

    add loading status and animation in button

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -11467,6 +11467,7 @@ exports.default = void 0;
 //
 //
 //
+//
 var _default = {
   props: {
     icon: {},
@@ -11502,6 +11503,8 @@ exports.default = _default;
       _vm.icon
         ? _c("g-icon", { staticClass: "icon", attrs: { name: _vm.icon } })
         : _vm._e(),
+      _vm._v(" "),
+      _c("g-icon", { staticClass: "loading", attrs: { name: "loading" } }),
       _vm._v(" "),
       _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
     ],
--- src/button.vue
+++ src/button.vue
@@ -2,6 +2,7 @@
       <button class="g-button" :class="{[`icon-${iconposition}`]:true}">
         <!-- <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg> -->
         <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
           <slot></slot>
         </div>
@@ -22,6 +23,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+@keyframes spin {
+  0% { transform: rotate(0deg);}
+  100% {transform: rotate(360deg);}
+}
 .g-button{
         font-size: var(--font-size);
         height: var(--button-height);
@@ -60,6 +65,9 @@ export default {
           order:1;
         }
       }
+      .loading {
+        animation: spin 2s infinite linear;
       }
+  }
 
 </style>
