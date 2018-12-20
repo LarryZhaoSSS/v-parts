796d41c70510571281425670089f60f0eccf22db
LarryKameZhao <larryzhao1994@outlook.com>
2018-12-20 08:25:55 +0800

    check element in g-button-group is g-button element or not

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -11635,7 +11635,37 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+var _default = {
+  mounted: function mounted() {
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = this.$el.children[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var node = _step.value;
+        var name = node.nodeName.toLowerCase();
+
+        if (name !== 'button') {
+          consle.warn("g-button-group\u7684\u5B50\u5143\u7D20\u5E94\u8BE5\u5168\u662Fg-button,\u4F46\u4F60\u5199\u7684\u662F".concat(name));
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
+};
 exports.default = _default;
         var $e7a02c = exports.default || module.exports;
       
--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,14 @@
 </template>
 <script>
 export default {
-
+  mounted () {
+    for (let node of this.$el.children){
+      let name = node.nodeName.toLowerCase()
+      if(name !== 'button'){
+        consle.warn(`g-button-group的子元素应该全是g-button,但你写的是${name}`)
+      }
+    }
+  }
 }
 </script>
 
