e81f2358f20e1b680e18c547c39ca94eccfa2761
LarryZhao <larryzhao1994@outlook.com>
2019-01-09 00:04:55 +0800

    g-input style

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-input', Input)
 new Vue({
   el:'#app',
   data () {
@@ -15,85 +17,3 @@ new Vue({
 })
 
 //unit test
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-{
-  const Constructor = Vue.extend(Button)
-  const button = new Constructor ({
-    propsData: {
-      icon:'setting'
-    }
-  })
-  button.$mount('#test')
-  let useElement = button.$el.querySelector('use')
-  console.log(useElement)
-  expect(useElement.getAttribute('xlink:href')).to.eq('#i-setting')
-  button.$el.remove()
-  button.$destroy()
-}
-{
-
-  const Constructor = Vue.extend(Button)
-  const button = new Constructor ({
-    propsData: {
-      icon: 'setting',
-      loading: true
-    }
-  })
-  button.$mount()
-  let useElement = button.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const button = new Constructor ({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  button.$mount(div)
-  let svg = button.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const button = new Constructor ({
-    propsData: {
-      icon: 'setting',
-      iconposition:'right'
-    }
-  })
-  button.$mount(div)
-  let svg = button.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  button.$el.remove()
-  button.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const gButton = new Constructor ({
-    propsData: {
-      icon: 'setting',
-      iconposition:'right'
-    }
-  })
-  gButton.$mount()
-  let spy = chai.spy(function(){})
-  gButton.$on('click', spy)
-  let button = gButton.$el
-  button.click()
-  expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value="value" :disabled="disabled" :readonly="readonly" type="text">
+    <template v-if="error">
+      <Icon  name="error" class="icon-error"></Icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+<script>
+import Icon from "./icon";
+export default {
+  components: {
+    Icon
+  },
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red:#f1453d;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  > * {
+      margin-right: .5em;
+      &:last-child{
+          margin-right: 0;
+      }
+  }
+  > input {
+    height: $height;
+    border-color: $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inside 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled],
+    &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  &.error {
+    > input {
+      border-color: #f1453d;
+    }
+  }
+  .icon-error{
+      fill:red;
+  }
+  .error-message{
+      color: $red;
+  }
+}
+</style>
