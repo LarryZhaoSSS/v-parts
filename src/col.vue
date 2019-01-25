78ad8d065a2b0c59d0b9bc25a042c561a38bb33b
LarryZhao <larryzhao1994@outlook.com>
2019-01-25 15:18:16 +0800

    使用计算属性重构row和col

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :style="colStyle" :class="colClass">
     <slot></slot>
   </div>
 </template>
@@ -9,6 +9,28 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  computed: {
+    colStyle () {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter /2  + 'px'
+      }
+    },
+    colClass () {
+      let { span, offset } = this
+      return [span && `col-${span}`, 
+              offset && `offset-${offset}`]
+    }
+  },
+  data () {
+    return {
+      gutter: 0,
+
     }
   }
 }
@@ -20,9 +42,16 @@ export default {
   width: 50%;
   border: 1px solid red;
   $class-prefix: col-;
+  padding: 0 10px;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,33 @@
 <template>
-  <div class="row">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  
+  name: 'VPartsRow',
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  computed: {
+    rowStyle() {
+      let { gutter } = this
+      return { marginLeft: -gutter / 2 + 'px', marginRight: gutter / 2 + 'px' }
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
+  }
+
 }
 </script>
 <style lang="scss" scoped>
 .row {
   display: flex;
+  /* margin: 0 -10px; */
 }
 </style>
