71a5ed858853642f037bc7b14bb68f0a93e1be88
LarryZhao <larryzhao1994@outlook.com>
2019-01-25 08:19:01 +0800

    g-row-col 雏形

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import Button from "./button"
 import ButtonGroup from "./button-group"
 import Icon from "./icon"
 import Input from "./input"
+import Row from "./row"
+import Col from "./col"
 Vue.component("g-button", Button)
 Vue.component("g-icon", Icon)
 Vue.component("g-button-group", ButtonGroup)
 Vue.component("g-input", Input)
+Vue.component("g-row", Row)
+Vue.component("g-col", Col)
 new Vue({
   el: "#app",
   data() {
