1ea7677bd5aee5e0fd859b651b2631ff63251bdc
LarryZhao <larryzhao1994@outlook.com>
2019-02-03 16:35:08 +0800

    only one toast can exist in page

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,24 @@
 import Toast from './toast'
+let currentToast
 export default {
   install (Vue, options) {
-    Vue.prototype.$toast = function (message, options) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData:options
-        
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+    Vue.prototype.$toast = function (message, toastOptions) {
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, message, propsData: toastOptions})
     }
   }
+}
+
+// helpers
+function createToast ({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({
+    propsData:propsData
+  })
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
