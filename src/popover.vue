eaec62bd39f2608bd90bf5b6dfe4269f437d4a9d
LarryZhao <larryzhao1994@outlook.com>
2019-02-19 16:57:13 +0800

    basic functions of the popover are finished

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -17,29 +17,40 @@ export default {
     }
   },
   methods: {
-    xxx() {
-      this.visible = !this.visible
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper)
-          let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
-          console.log(width, height, top, left)
-          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-          this.$refs.contentWrapper.style.top = top + window.scrollY  + 'px'
-          this.$refs.contentWrapper.style.width = width + 'px'
-          this.$refs.contentWrapper.style.height =  height + 'px'
-          let eventHandler = () => {
-            this.visible = false
-            document.removeEventListener('click', eventHandler)
-          }
-          document.addEventListener('click', eventHandler)
-        })
-
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper)
+      let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    },
+    onClickDocument(e) {
+      if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+        return
+      }
+      this.close()
+    },
+    open() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+    close() {
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        } else {
+          this.open()
+        }
       }
     }
   },
   mounted() {
-    console.log(this.$refs.triggerWrapper)
   }
 }
 </script>
@@ -48,12 +59,11 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  
 }
 .content-wrapper {
-    position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    transform: translateY(-100%);
-  }
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
 </style>
