af124c8fe6a597804ab9e16e718516e6454ce163
LarryZhao <larryzhao1994@outlook.com>
2019-02-21 14:38:26 +0800

    add scss vars

--- src/button.vue
+++ src/button.vue
@@ -31,27 +31,35 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$font-size: 14px;
+$button-height: 32px;
+$button-bg: white;
+$button-active-bg: white;
+$button-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
 @keyframes spin {
   0% { transform: rotate(0deg);}
   100% {transform: rotate(360deg);}
 }
 .g-button{
-        font-size: var(--font-size);
-        height: var(--button-height);
+        font-size: $font-size;
+        height: $button-height;
         padding: 0 1em;
         font:inherit;
-        border-radius: var(--button-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+        border-radius: $button-radius;
+        border: 1px solid $border-color;
+        background: $button-bg;
         display: inline-flex;
         justify-content: center;
         align-items: center;
         vertical-align: middle;
          &:hover{
-        border-color: var(--border-color-hover);
+        border-color: $border-color-hover;
       }
       &:hover{
-        background-color: var(--button-active-bg);
+        background-color: $button-active-bg;
       }
       &:focus{
         outline: none;
