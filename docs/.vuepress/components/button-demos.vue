1afd6ac6725f6520d99ebc6f38d742e95b46efac
LarryZhao <larryzhao1994@outlook.com>
2019-02-21 16:08:18 +0800

    the button's document

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -4,6 +4,11 @@
     <v-button icon="setting">设置按钮</v-button>
     <v-button icon="setting" :loading="isLoading" @click="isLoading = !isLoading">加载状态</v-button>
     <v-button icon="setting" iconposition="right">图标右侧</v-button>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <script>
@@ -12,9 +17,21 @@ export default {
   components: {
     vButton: VButton
   },
-  data () {
+  data() {
     return {
-      isLoading: true
+      isLoading: true,
+      content: `
+      <v-button>默认按钮</v-button>
+      <v-button icon="setting">设置按钮</v-button>
+      <v-button icon="setting" :loading="isLoading" @click="isLoading = !isLoading">加载状态</v-button>
+      <v-button icon="setting" iconposition="right">图标右侧</v-button>
+
+      data () {
+        return {
+          isLoading: true
+        }
+      }
+      `
     }
   }
 }
