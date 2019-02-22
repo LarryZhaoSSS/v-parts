ed93e9ef1c1577f1ce656ce2d0ebfecbeba1cfab
LarryZhao <larryzhao1994@outlook.com>
2019-02-22 14:21:44 +0800

    a bug on col

--- src/col.vue
+++ src/col.vue
@@ -23,52 +23,14 @@ export default {
     offset: {
       type: [Number, String]
     },
-    phone: {
-      type: Object,
-      validator
-    },
-    ipad: {
-      type: Object,
-      validator
-    },
-    narrowPc: {
-      type: Object,
-      validator
-    },
-    pc: {
-      type: Object,
-      validator
-    },
-    widePc: {
-      type: Object,
-      validator
-    }
-  },
-  computed: {
-    colStyle() {
-      return {
-        paddingLeft: this.gutter / 2 + 'px',
-        paddingRight: this.gutter / 2 + 'px'
-      }
-    },
-    colClass() {
-      let { span, offset, phone, ipad, narrowPc, pc, widePc } = this
-      let phoneClass = []
-      let createClasses = this.createClasses
-      console.log('in class')
-       return [
-          ...createClasses({span, offset}),
-          ...createClasses(ipad, 'ipad-'),
-          ...createClasses(narrowPc, 'narrow-pc-'),
-          ...createClasses(pc, 'pc-'),
-          ...createClasses(widePc, 'wide-pc-'),
-       ]
-    }
+    ipad: { type: Object, validator, },
+    narrowPc: { type: Object, validator, },
+    pc: { type: Object, validator, },
+    widePc: { type: Object, validator, }
   },
   data() {
     return {
       gutter: 0,
-
     }
   },
   methods: {
@@ -77,85 +39,100 @@ export default {
       let array = []
       if (obj.span) { array.push(`col-${str}${obj.span}`) }
       if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
-      console.log('class------------')
-      console.log(array)
       return array
+    }
+  },
+  computed: {
+    colClass() {
+      let { span, offset, ipad, narrowPc, pc, widePc } = this
+      let createClasses = this.createClasses
+      return [
+        ...createClasses({ span, offset }),
+        ...createClasses(ipad, 'ipad-'),
+        ...createClasses(narrowPc, 'narrow-pc-'),
+        ...createClasses(pc, 'pc-'),
+        ...createClasses(widePc, 'wide-pc-'),
+      ]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px',
+      }
     }
   }
 }
 </script>
-<style lang="scss" scoped>
- .col {
-    $class-prefix: col-;
-    border: 1px solid red;
-    background: grey;
-    height: 100px;
+<style scoped lang="scss">
+.col {
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
+    }
+  }
+  @media (min-width: 577px) {
+    $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
       }
     }
-    $class-prefix: offset-;
+    $class-prefix: offset-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 577px) {
-      $class-prefix: col-ipad-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-ipad-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
+  }
+  @media (min-width: 769px) {
+    // 770
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
       }
     }
-    @media (min-width: 769px){ // 770
-      $class-prefix: col-narrow-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-narrow-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
-    @media (min-width: 993px) {
-      $class-prefix: col-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
-    @media (min-width: 1201px) {
-      $class-prefix: col-wide-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-wide-pc-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
+  @media (min-width: 993px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -7,46 +7,45 @@
 export default {
   name: 'VPartsRow',
   props: {
-    gutter: {
-      type: [Number, String]
-    },
-    align: {
-      type: String,
-      validator(value) {
-        return ['left', 'right', 'center']
+      gutter: {
+        type: [Number, String]
+      },
+      align: {
+        type: String,
+        validator (value) {
+          return ['left', 'right', 'center'].indexOf(value) >= 0
+        }
       }
-    }
-  },
-  computed: {
-    rowStyle() {
-      let { gutter } = this
-      return { marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }
     },
-    rowClass() {
-      let { align } = this
-      return [align && `align-${align}`]
+    computed: {
+      rowStyle () {
+        let {gutter} = this
+        return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+      },
+      rowClass () {
+        let {align} = this
+        return [align && `align-${align}`]
+      }
+    },
+    mounted () {
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
     }
-  },
-  mounted() {
-    this.$children.forEach((vm) => {
-      vm.gutter = this.gutter
-    })
   }
-
-}
 </script>
-<style lang="scss" scoped>
-.row {
-  display: flex;
-  flex-wrap: wrap;
-  &.align-left {
-    justify-content: flex-start;
+<style scoped lang="scss">
+  .row{
+    display: flex;
+    flex-wrap: wrap;
+    &.align-left {
+      justify-content: flex-start;
+    }
+    &.align-right {
+      justify-content: flex-end;
+    }
+    &.align-center {
+      justify-content: center;
+    }
   }
-  &.align-right {
-    justify-content: flex-end;
-  }
-  &.align-center {
-    justify-content: center;
-  }
-}
 </style>
