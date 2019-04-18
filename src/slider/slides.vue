16897ec81769d5ba0f744841fd6598b33373d1fd
LarryKameZhao <larryzhao1994@outlook.com>
2019-04-18 23:18:55 +0800

    support slide left and right on mobile

--- src/slider/slides.vue
+++ src/slider/slides.vue
@@ -2,6 +2,9 @@
   <div class="vparts-slides"
        @mouseenter="onMouseEnter"
        @mouseleave="onMouseLeave"
+       @touchstart="onTouchStart"
+       @touchmove="onTouchMove"
+       @touchend="onTouchEnd"
   >
     <div class="vparts-slides-window">
       <div class="vparts-slides-wrapper">
@@ -31,7 +34,8 @@
     },
     computed: {
       selectedIndex() {
-        return this.names.indexOf(this.selected) || 0
+        let index = this.names.indexOf(this.selected)
+        return index === -1?0 : index
       },
       names() {
         return this.$children.map(vm => vm.name)
@@ -42,10 +46,41 @@
       return {
         childrenLength: 0,
         lastSelectedIndex: undefined,
-        timerId: undefined
+        timerId: undefined,
+        startTouch:undefined,
       }
     },
     methods: {
+      onTouchStart(e) {
+        this.pause()
+        if (e.touches.length > 1) {
+          return
+        }
+        this.startTouch = e.touches[0]
+      },
+      onTouchMove() {
+
+      },
+      onTouchEnd(e) {
+        let endTouch = e.changedTouches[0]
+        let {clientX:x1,clientY:y1} = this.startTouch
+        let {clientX:x2, clientY:y2} = endTouch
+        if(endTouch.clientX >  this.startTouch.clientX) {
+          console.log('right')
+        }
+        let distance = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2))
+        let deltaY = Math.abs(y2-y1)
+        let rate = distance / deltaY
+        if (rate > 2) {
+          console.log('在滑动')
+          if (x2 > x1) {
+            this.select(this.selectedIndex - 1)
+          } else {
+            this.select(this.selectedIndex + 1)
+          }
+        }
+        this.playAutomatically()
+      },
       playAutomatically() {
         if (this.timerId) {
           return
@@ -75,8 +110,15 @@
       onMouseLeave() {
         this.playAutomatically()
       },
-      select(index) {
-        this.$emit('update:selected', this.names[index])
+      select(newIndex) {
+        this.lastSelectedIndex = this.selectedIndex
+        if (newIndex === -1) {
+          newIndex = this.names.length + 1
+        }
+        if (newIndex === this.names.length) {
+          newIndex = 0
+        }
+        this.$emit('update:selected', this.names[newIndex])
       },
       getSelected() {
         let first = this.$children[0]
@@ -86,10 +128,10 @@
         let selected = this.getSelected()
         this.$children.forEach((vm) => {
           let reverse = this.selectedIndex > this.lastSelectedIndex ? false : true
-          if(this.lastSelectedIndex === this.$children.length-1&&this.selectedIndex===0) {
+          if (this.lastSelectedIndex === this.$children.length - 1 && this.selectedIndex === 0) {
             reverse = false
           }
-          if(this.lastSelectedIndex ===0 &&this.selectedIndex===this.$children.length-1) {
+          if (this.lastSelectedIndex === 0 && this.selectedIndex === this.$children.length - 1) {
             reverse = true
           }
           vm.reverse = reverse
@@ -124,6 +166,7 @@
       display: flex;
       justify-content: center;
       align-items: center;
+      
       > span {
         width: 20px;
         height: 20px;
@@ -134,13 +177,16 @@
         margin: 0 8px;
         background: #ddd;
         font-size: 12px;
-        &:hover{
+        
+        &:hover {
           cursor: pointer;
         }
+        
         &.active {
           background: black;
           color: white;
-          &:hover{
+          
+          &:hover {
             cursor: pointer;
           }
         }
