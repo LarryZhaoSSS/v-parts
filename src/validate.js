cd85c231acb6a8b4c128fbda5e7d04054bad94f4
LarryKameZhao <larryzhao1994@outlook.com>
2019-04-24 08:05:47 +0800

    add required pattern minLength tdd cases

--- src/validate.js
+++ src/validate.js
@@ -3,15 +3,61 @@ export default function validate(data, rules) {
   rules.forEach((rule) => {
     let value = data[rule.key]
     if (rule.required) {
-      if (value !== 0 && !value) {
-        errors[rule.key] = {required: '必填'}
+      let error = validate.required(value)
+      if (error) {
+        ensureObject(errors, rule.key)
+        errors[rule.key].required = error
+        return
       }
+      // if (value !== 0 && !value) {
+      //   errors[rule.key] = {required: '必填'}
+      //   return
+      // }
     }
-    if(rule.pattern) {
-      if(rule.pattern.test(value)=== false){
-        errors[rule.key] = {pattern:'邮箱格式不正确'}
+    if (rule.pattern) {
+      let error = validate.pattern(value, rule.pattern)
+      if (error) {
+        ensureObject(errors, rule.key)
+        errors[rule.key].pattern = error
+      }
+      // if (rule.pattern === 'email') {
+      //   rule.pattern = /^.+@.+$/
+      // }
+      // if (rule.pattern.test(value) === false) {
+      //   ensureObject(errors, rule.key)
+      //   errors[rule.key].pattern = '邮箱格式不正确'
+      // }
+    }
+    if (rule.minLength) {
+      if (value.length < rule.minLength) {
+        ensureObject(errors, rule.key)
+        errors[rule.key].minLength = '邮箱太短'
       }
     }
   })
   return errors
+}
+validate.required = (value) => {
+  if (value !== 0 && !value) {
+    return '必填'
+  }
+}
+validate.pattern = (value, pattern) => {
+  if (pattern === 'email') {
+    pattern = /^.+@.+$/
+  }
+  if (pattern.test(value) === false) {
+    return '邮箱格式不正确'
+  }
+}
+validate.minLength = (value, minLength) => {
+  if (value.length < minLength) {
+    return '邮箱太短'
+  }
+}
+
+function ensureObject(obj, key) {
+  if (typeof obj[key] !== 'object') {
+    obj[key] = {}
+  }
 }
--- tests/unit/validate.spec.js
+++ tests/unit/validate.spec.js
@@ -7,7 +7,7 @@ describe('validate',()=>{
   it('存在',()=>{
     expect(validate).to.exist
   })
-  it('test validate1',()=>{
+  it('required true报错',()=>{
     let data = {
       email:''
     }
@@ -18,7 +18,7 @@ describe('validate',()=>{
    let errors =  validate(data, rules)
     expect(errors.email.required).to.eq('必填')
   })
-  it('test validate number is 0',()=>{
+  it('required true 通过',()=>{
     let data = {
       email:0
     }
@@ -29,7 +29,7 @@ describe('validate',()=>{
     let errors =  validate(data, rules)
     expect(errors.email).to.not.exist
   })
-  it('test regular expression',()=>{
+  it('pattern 报错',()=>{
     let data = {
       email:'@qq.com'
     }
@@ -40,7 +40,7 @@ describe('validate',()=>{
     let errors =  validate(data, rules)
     expect(errors.email.pattern).to.eq('邮箱格式不正确')
   })
-  it('test regular email expression correct',()=>{
+  it('pattern 通过',()=>{
     let data = {
       email:'1@qq.com'
     }
@@ -51,4 +51,54 @@ describe('validate',()=>{
     let errors =  validate(data, rules)
     expect(errors.email).to.not.exist
   })
+  it('pattern email 报错',()=>{
+    let data = {
+      email:'@qq.com'
+    }
+    let rules = [{
+      key:'email',
+      pattern: 'email'
+    }]
+    let errors =  validate(data, rules)
+    expect(errors.email.pattern).to.eq('邮箱格式不正确')
+  })
+  it('pattern email 通过',()=>{
+    let data = {
+      email:'1@qq.com'
+    }
+    let rules = [{
+      key:'email',
+      pattern: 'email'
+    }]
+    let errors =  validate(data, rules)
+    expect(errors.email).to.not.exist
+  })
+  it('pattern and required',()=>{
+    let data = {
+      email:''
+    }
+    let rules = [{
+      key:'email',
+      pattern: 'email',
+      required: true
+    }]
+    let errors =  validate(data, rules)
+    expect(errors.email.required).to.exist
+    expect(errors.email.pattern).to.not.exist
+  })
+  it('pattern & minLength',()=>{
+    let data = {
+      email:'1@3'
+    }
+    let rules = [{
+      key:'email',
+      pattern: 'email',
+      minLength:4,
+      required: true
+    }]
+    let errors =  validate(data, rules)
+    expect(errors.email.minLength).to.exist
+    expect(errors.email.pattern).to.not.exist
+  })
 })
+
