12828584082168ba9b03762fd684bb527f83d41d
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-21 23:44:19 +0800

    完成上传进度

--- src/uploader.vue
+++ src/uploader.vue
@@ -6,9 +6,12 @@
     <div ref="temp" style="width:0;height:0;overflow:hidden;"></div>
     <ol>
       <li v-for="file in fileList" :key="file.name">
-        <img :src="file.url" width="100" height="100" alt="">
-        {{ file.name }}
-        <button @click="onRemoveFile(file)">X</button>
+        <template v-if="file.status === 'uploading'">菊花</template>
+        <template v-else>
+          <img :src="file.url" width="100" height="100" alt="">
+          {{ file.name }}
+          <button @click="onRemoveFile(file)">X</button>
+        </template>
       </li>
     </ol>
   </div>
@@ -70,23 +73,63 @@
         this.$refs.temp.appendChild(input)
         return input
       },
-      uploadFile(file) {
+      beforeUploadFile(rawFile,newName) {
+        let {type,size} = rawFile
+        console.log(this.fileList)
+        this.$emit('update:fileList',[...this.fileList,{name:newName,type,size,status:'uploading'}])
+
+      },
+      afterUploadFile(rawFile,newName,url) {
+        let {size,type} = rawFile
+        console.log(file)
+        let file = this.fileList.filter(f=>f.name===newName)[0]
+        let index = this.fileList.indexOf(file)
+        console.log(this.fileList)
+        console.log(file)
+        let copy = JSON.parse(JSON.stringify(file))
+        copy.url = url
+        copy.status = 'success'
+        let fileListCopy = [...this.fileList]
+        fileListCopy.splice(index,1,copy)
+        console.log('---update:fileList---')
+        console.log(this.fileList)
+        this.$emit('update:fileList',fileListCopy)
+      },
+      uploadFile(rawFile) {
+        let {name, size, type} = rawFile
+        let newName = this.generateName(name)
+        this.beforeUploadFile(rawFile, newName)
         let formData = new FormData()
-        formData.append(this.name, file)
-        let {name, size, type} = file
+        formData.append(this.name, rawFile)
         this.postRequest(formData,(response)=>{
           let url = this.parseResponse(response)
           this.url = url
-          while(this.fileList.filter(f=>f.name===name).length>0) {
-            let dotIndex = name.lastIndexOf('.')
-            let prefix = name.substring(0,dotIndex)
-            let suffix = name.substring(dotIndex)
-            name = prefix + '(1)' +suffix
-          }
-          this.$emit('update:fileList',[...this.fileList,{name,type,size,url}])
+          this.afterUploadFile(rawFile,newName,url)
+        },()=>{
+          this.uploadError(newName)
         })
       },
-      postRequest(formData,success) {
+      uploadError(newName) {
+        let file = this.fileList.filter(f=>f.name === newName)[0]
+        let index = this.fileList.indexOf(file)
+        let fileCopy = JSON.parse(JSON.stringify(file))
+        fileCopy.status = 'fail'
+        let fileListCopy = [...this.fileList]
+        fileListCopy.splice(index , 1, fileCopy)
+        this.$emit('update:fileList',fileListCopy)
+      },
+      generateName (name) {
+        while(this.fileList.filter(f=>f.name===name).length>0) {
+          let dotIndex = name.lastIndexOf('.')
+          let prefix = name.substring(0,dotIndex)
+          let suffix = name.substring(dotIndex)
+          name = prefix + '(1)' +suffix
+        }
+        return name
+      },
+      
+      postRequest(formData,success,fail) {
+        
         let xhr = new XMLHttpRequest()
         xhr.open(this.method, this.action)
         xhr.onload = ()=>{
